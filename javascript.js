4d60014486443ad912e20a69ee25d2ff4526bf82
Natalie Roode <nroode@gmail.com>
2018-06-20 13:20:43 -0700

    Added date and time to landing page; added random background image; added icons as navigation

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+function getDateTime() {
+
+    var currentDate = new Date();
+
+    var day = currentDate.getDay();
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var date = currentDate.getDate();
+
+    var month = currentDate.getMonth().toString();
+    var monthNames = [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+
+    var year = currentDate.getFullYear();
+
+    var fullDate = dayNames[day] + " " + monthNames[month] + " " + date + ", " + year;
+
+    var hour = currentDate.getHours();
+    if (hour<10) {
+        hour = "0" + hour;
+    }
+
+
+    var minutes = currentDate.getMinutes();
+
+    if (minutes<10) {
+        minutes = "0" + minutes;
+    }
+
+    var time = hour + ":" + minutes;
+
+    document.querySelector('.date').textContent = fullDate;
+    document.querySelector('.time').textContent = time;
+
+    setTimeout(getDateTime, 30000);
+
+};
+
+getDateTime();
