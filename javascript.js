6dfd473b1564823bbb82f9183ff48b197f5211b4
Natalie Roode <nroode@gmail.com>
2018-07-05 23:35:24 -0700

    formatted news date; increased close button size

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
 
 
-//----------DATE AND TIME -------------
+//----------DATE AND TIME -------------------------------------
 
 function getDateTime() {
 
@@ -18,7 +18,6 @@ function getDateTime() {
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     var date = currentDate.getDate();
-
     var month = currentDate.getMonth().toString();
     var monthNames = [
         "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
@@ -42,26 +41,6 @@ function getDateTime() {
 
     var time = hour + ":" + minutes;
 
-    // console.log(month);
-    // console.log(date);
-    // console.log(year);
-    // console.log(time);
-
-
-                // var todayDate = function (month, date, year, time) {
-                //     this.month = month;
-                //     this.date = date;
-                //     this.year = year;
-                //     this.time = time;
-                //}
-
-                // var todayDate = {
-                //     this.month = month;
-                //     this.date = date;
-                //     this.year = year;
-                //     this.time = time;
-                //}
-            
 
     document.querySelector('.date').textContent = fullDate;
     document.querySelector('.time').textContent = time;
@@ -73,7 +52,7 @@ function getDateTime() {
 getDateTime();
 
 
-//---------NEWS ---------
+//---------NEWS -------------------------------------
 
 //key 393bdf4f125f4a6298675ec35996dccd
 
@@ -99,20 +78,23 @@ function getNews() {
                 var title = articles[i].title;
                 var timePublished = articles[i].publishedAt;
 
-                // convertDate();
+
+                var APIDate = timePublished.split('T');
+                var APIDateArr = APIDate[0].split('-');
+                var APIMonth = Number(APIDateArr[1], 10) - 1;
+
+                var abrevMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+                var newsDate = abrevMonths[APIMonth] + " " + APIDateArr[2] + ", " + APIDateArr[0]
+
 
                 var source = articles[i].source.name;
                 var articleLink = articles[i].url;
     
-                // console.log(title);
-                // console.log(timePublished);
-                // console.log(source);
-
 
                 myLi += '<li class="article"><div class="article-info"><a href="' + articleLink + 
                 '"><ul class="article-info-list"><li class="article-title">' + title + 
                 '</li><li><span class="article-source">' + source + 
-                ' | </span><span class="article-time">'+ timePublished + 
+                ' | </span><span class="article-time">'+ newsDate + 
                 '</span></li></ul></a></div></li>'
             };
             
@@ -124,63 +106,17 @@ getNews();
 
 
 var newsIcon = document.querySelector(".icon-svg142").addEventListener("click", displayNews);
-
 function displayNews() {
     document.querySelector('.feature-news').style.display = "block";
     // document.querySelector('.feature-news').classList.add('.feature-news-visible');
 }
 
 var newsCloseBtn = document.querySelector(".news-close-btn").addEventListener("click", closeNews);
-
 function closeNews() {
     document.querySelector('.feature-news').style.display = "none";
 }
 
 
 
-// getNews();
-
-// function convertDate() {
-
-//     Mon Jul 02 2018 11:04:04 GMT-0700 (PDT)  in JS
-//     2018-07-02T17:40:46Z  from API
 
 
-
-// };
-
-
-// return {
-    //     month: month,
-    //     date: date,
-    //     year: year,
-    //     time: time,
-// }
-
-
-
-
-
-
-
-
-
-
-// var addListItem = function (obj) {
-//     var html, newHtml, element;  class element from html
-
-    
-    
-//     element = DOMstrings.incomeContainer;
-//     html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
-    
-
-//     replace the placeholder text with actual data
-
-//     newHtml = html.replace('%id%', obj.id);
-//     newHtml = newHtml.replace('%description%', obj.description);
-//     newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
-
-//     insert the html into the dom
-//     document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-// };
