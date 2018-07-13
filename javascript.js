315c3278a8b65432ee98d51847ae44afaa60a165
tyre king <tyr3.k1ng@gmail.com>
2018-07-13 00:48:30 -0400

    Trash can now deletes when clicked, plus now adds when clicked

--- javascript.js
+++ javascript.js
@@ -137,30 +137,51 @@ function updateTitle(){
     document.getElementById("songTitle").innerHTML = title;
 };
 
+
 function plus(){
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("userInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("taskList").appendChild(li);
-    }
-    document.getElementById("userInput").value = "";
-    
-    var span = document.createElement("SPAN");
+   
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("userInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === '') {
+          alert("You must write something!");
+        } else {
+          document.getElementById("taskList").appendChild(li);
+        }
+        document.getElementById("userInput").value = "";
+      
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("");
+        span.className =  "fas fa-trash-alt";
+        span.appendChild(txt);
+        li.appendChild(span);
+      
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+      };
+
+      
+var myNodelist = document.getElementsByClassName("task");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
   var txt = document.createTextNode("");
-  span.className = "fas fa-trash-alt";
+  span.className =  "fas fa-trash-alt";
   span.appendChild(txt);
-  li.appendChild(span);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("fas fa-trash-alt");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
-function complete(){
-    var x = document.getElementById("taskList");
-    if(x.style.display === "None"){
-        x.style.display = "block";
-    }else{
-        x.style.display = "none";
-    }
-}
+
