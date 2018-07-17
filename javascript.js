74f05c924db611fdfeb1e4b27494f52b868dfb40
tyre king <tyr3.k1ng@gmail.com>
2018-07-17 19:28:30 -0400

    Delete Li elements instead of hiding them.

--- javascript.js
+++ javascript.js
@@ -290,8 +290,9 @@ var close = document.getElementsByClassName("fas fa-trash-alt");
 var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
+        // var div = this.parentElement;
+        // div.style.display = "none";
+        close.parentNode.removeChild(close);
     }
 }
 
